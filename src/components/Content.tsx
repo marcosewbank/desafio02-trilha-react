6726372df16f9cf498bb0d052365f821fe530046
marcosewbank <marcos.ewbank@acct.global>
2021-03-22 17:57:07 -0300

    refactoring watch me component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+import "../styles/content.scss";
+
+export function Content({ movies, selectedGenre }: any) {
+  console.log("🚀 ~ file: Content.tsx ~ line 2 ~ Content ~ movies", movies);
+
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <div className="container">
+        <header>
+          <span className="category">
+            Categoria:<span> {selectedGenre.title}</span>
+          </span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map((movie) => (
+              <MovieCard
+                key={movie.imdbID}
+                title={movie.Title}
+                poster={movie.Poster}
+                runtime={movie.Runtime}
+                rating={movie.Ratings[0].Value}
+              />
+            ))}
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 }
